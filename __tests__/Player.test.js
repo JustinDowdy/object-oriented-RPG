7fab447747ad74abb0bec655e0386dbbcd735ede
Justin Dowdy <83055639+JustinDowdy@users.noreply.github.com>
2021-08-12 17:28:45 -0500

    added prototypes to methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,7 @@ const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
+// console.log(new Potion());
 
 const Player = require('../lib/Player');
 
@@ -18,3 +18,21 @@ test('creates a player object', () => {
     );
 });
 
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
